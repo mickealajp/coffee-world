c917753ff975b2d1535637d32160d5a9877a95da
mickealajp <124074359+mickealajp@users.noreply.github.com>
2023-09-29 13:52:56 -0400

    adds menu items and tags to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,19 @@ import logo from "../../assets/coffee-logo.jpeg";
 
 function Header () {
     return (
-      <section>
-        <h1>Coffee World</h1>
-        <img src={logo} alt="coffee"></img>
-      </section>
+      <div className="header">
+        <div className="coffee">
+          <h1 className="coffee-title">Coffee World</h1>
+          <img className="coffee-logo" src={logo} alt="coffeebeans"></img>
+        </div>
+        <div className="menu">
+          <ul className="menu-items">
+            <li className="menu-select">Coffee</li>
+            <li className="menu-select">Bakery</li>
+            <li className="menu-select">Merch</li>
+          </ul>
+        </div>
+      </div>
     );
 }
 
